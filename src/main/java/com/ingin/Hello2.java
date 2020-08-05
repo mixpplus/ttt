5dcbc56c40d2e21554e556234a3705081d726f8d
ingin <mixpplus@gmail.com>
2020-08-05 20:20:59 +0800

    首次提交

---
+++ src/main/java/com/ingin/Hello2.java
@@ -0,0 +1,53 @@
+package com.ingin;
+
+import cn.hutool.core.util.StrUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hello2 {
+
+    public static void main(String[] args) {
+        if (StrUtil.isEmpty("he")) {
+            System.out.println("hello");
+        }
+
+        List<String> list = new ArrayList<>();
+        list.add("hello");
+        list.add("hello2");
+        String collect = String.join("-", list);
+        System.out.println(collect);
+        String format = String.format("%s-%s", "ni", "hao");
+        System.out.println(format);
+
+
+        if (StrUtil.isEmpty("he")) {
+            System.out.println("hello");
+        }
+
+        List<String> list1 = new ArrayList<>();
+        list.add("hello");
+        list.add("hello2");
+        String collect1 = String.join("-", list);
+        System.out.println(collect);
+        String format1 = String.format("%s-%s", "ni", "hao");
+        System.out.println(format);
+
+
+
+        if (StrUtil.isEmpty("he")) {
+            System.out.println("hello");
+        }
+
+        List<String> list2 = new ArrayList<>();
+        list.add("hello");
+        list.add("hello2");
+        String collect2 = String.join("-", list);
+        System.out.println(collect);
+        String format2 = String.format("%s-%s", "ni", "hao");
+        System.out.println(format);
+
+    }
+
+
+}
